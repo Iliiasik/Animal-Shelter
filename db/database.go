0be24ea537638334225ca6cdd5d8f61bf115cdab
Iliias Bayishev <168758422+Iliiasik@users.noreply.github.com>
2024-11-19 17:44:00 +0600

    Главная страница форума. Финиш

--- db/database.go
+++ db/database.go
@@ -2,14 +2,26 @@ package db
 
 import (
 	"Animals_Shelter/models"
+	"fmt"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"os"
 )
 
 func ConnectDB() *gorm.DB {
-	dsn := "user=postgres password=root dbname=AShelter sslmode=disable client_encoding=UTF8"
+	err := godotenv.Load("configuration.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+	dsn := fmt.Sprintf(
+		"user=%s password=%s dbname=%s sslmode=disable client_encoding=UTF8",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
