c930e18b4b57c793652ed270003660e9c469943f
Elzar4816 <elzar.kurmanbekov@gmail.com>
2024-11-27 19:01:31 +0600

    Докеризация проекта

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+# Используем официальный образ Golang как базовый для сборки
+FROM golang:1.23 as builder
+
+# Устанавливаем рабочую директорию
+WORKDIR /app
+
+# Копируем файлы проекта
+COPY go.mod go.sum ./
+RUN go mod download
+COPY . .
+
+# Сборка приложения
+RUN go build -o main .
+
+# Используем минимальный образ для запуска
+FROM debian:bookworm-slim
+
+# Устанавливаем необходимые пакеты, включая ca-certificates
+RUN apt-get update && apt-get install -y \
+    ca-certificates \
+    && rm -rf /var/lib/apt/lists/*
+
+# Устанавливаем рабочую директорию
+WORKDIR /app
+
+# Копируем все файлы из сборочного контейнера в текущую рабочую директорию
+COPY --from=builder /app /app
+
+# Открываем порт
+EXPOSE 8080
+
+# Запускаем приложение
+CMD ["./main"]
--- admin/db_old/db_old.go
+++ admin/db_old/db_old.go
@@ -4,25 +4,37 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/joho/godotenv"
 	"log"
 	"os"
 )
 
 func ConnectOldDB() *gorm.DB {
-	err := godotenv.Load("configuration.env")
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
+	// Проверяем наличие всех необходимых переменных окружения
+	requiredEnvVars := []string{"DB_USER", "DB_PASSWORD", "DB_NAME", "DB_HOST", "DB_PORT"}
+	for _, envVar := range requiredEnvVars {
+		if os.Getenv(envVar) == "" {
+			log.Fatalf("Environment variable %s is not set", envVar)
+		}
 	}
+
+	// Формируем строку подключения
 	dsn := fmt.Sprintf(
-		"host=localhost user=%s password=%s dbname=%s sslmode=disable",
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 	)
+
+	// Подключаемся к базе данных
 	db, err := gorm.Open("postgres", dsn)
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
 	}
+
+	// Логируем успешное подключение
+	log.Println("Successfully connected to the database")
+
 	return db
 }
