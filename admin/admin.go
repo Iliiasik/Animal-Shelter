19aa8e6c697afd36628d9c0cbb5595549ed41999
Elzar4816 <elzar.kurmanbekov@gmail.com>
2024-12-04 20:59:04 +0600

    Попытка реализовать корректное отображение username в таблицах. Проблема пока в том что невозможно нормально редактировать данные в которых я модифицирую отображение username. Да и в целом в таблицах связанных с юзером как то непонятно как редактировать данные

--- admin/admin.go
+++ admin/admin.go
@@ -2,16 +2,17 @@ package admin
 
 import (
 	"Animals_Shelter/models"
+	"fmt"
 	"github.com/jinzhu/gorm"
 	"github.com/qor/admin"
-	"github.com/qor5/admin/v3/presets"
+	"github.com/qor/qor"
 )
 
 // InitAdmin инициализирует и возвращает QOR Admin
 func InitAdmin(db *gorm.DB) *admin.Admin {
 	// Создаём новый экземпляр QOR Admin
 	Admin := admin.New(&admin.AdminConfig{DB: db})
-	pb := presets.New()
+
 	// Регистрируем ресурсы
 	Admin.AddResource(&models.Adoption{})
 	Admin.AddResource(&models.AdoptionStatus{})
@@ -28,30 +29,48 @@ func InitAdmin(db *gorm.DB) *admin.Admin {
 	Admin.AddResource(&models.Role{})
 	Admin.AddResource(&models.Session{})
 	Admin.AddResource(&models.Topic{})
-	Admin.AddResource(&models.UserDetail{})
-	Admin.AddResource(&models.UserEmailConfirmation{})
-	Admin.AddResource(&models.UserImage{})
 	Admin.AddResource(&models.User{})
-
-	// Добавляем кастомный дашборд в меню
-	Admin.AddMenu(&admin.Menu{
-		Name: "Dashboard",
-		Link: "/admin/dashboard",
+	Admin.AddResource(&models.UserDetail{}, &admin.Config{}).Meta(&admin.Meta{
+		Name:  "User",
+		Label: "Username",
+		Type:  "string",
+		Valuer: func(record interface{}, context *qor.Context) (result interface{}) {
+			if userDetail, ok := record.(*models.UserDetail); ok {
+				// Используем Where для загрузки данных конкретного пользователя
+				if err := db.Where("id = ?", userDetail.UserID).First(&userDetail.User).Error; err != nil {
+					fmt.Printf("Ошибка загрузки пользователя: %v\n", err)
+				}
+				fmt.Printf("User: %+v\n", userDetail.User)
+				if userDetail.User.Username != "" {
+					return userDetail.User.Username
+				}
+			}
+			return "Not found"
+		},
 	})
 
-	// Обработка маршрута для дашборда
-	Admin.GetRouter().Get("/dashboard", func(ctx *admin.Context) {
-		// Пример кастомного HTML контента
-		ctx.Writer.Write([]byte(`
-			<h1>Дашборд</h1>
-			<p>Добро пожаловать в ваш кастомный дашборд!</p>
-			<div>
-				<a href="/admin/resources/animals">Список животных</a>
-				<a href="/admin/resources/users">Пользователи</a>
-			</div>
-		`))
+	Admin.AddResource(&models.UserEmailConfirmation{})
+
+	Admin.AddResource(&models.UserImage{}, &admin.Config{
+		Menu: []string{"User Images"},
+	}).Meta(&admin.Meta{
+		Name:  "User",
+		Label: "Username",
+		Type:  "string",
+		Valuer: func(record interface{}, ctx *qor.Context) interface{} {
+			if userImage, ok := record.(*models.UserImage); ok {
+				// Загружаем данные о пользователе
+				if err := db.Model(userImage).Preload("User").First(userImage).Error; err != nil {
+					fmt.Printf("Ошибка загрузки пользователя: %v\n", err)
+				}
+				fmt.Printf("UserImage: %+v\n", userImage.User) // Логируем содержимое
+				if userImage.User.Username != "" {
+					return userImage.User.Username
+				}
+			}
+			return "Not found"
+		},
 	})
-	ConfigTopicDashboard(pb, db)
 
 	return Admin
 }
