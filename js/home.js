061eca9bbed78c5d4c33cbf4ba47f4c3ef98fb57
Elzar4816 <elzar.kurmanbekov@gmail.com>
2024-12-07 11:04:50 +0600

    sweetAlert об успешном логине. Также вытащил js из home.html как home.js

---
+++ js/home.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll('.carousel-image');
+    let index = 0;
+
+    function showNextImage() {
+        images[index].style.opacity = 0; // Скрываем текущее изображение
+        index = (index + 1) % images.length; // Переход к следующему изображению
+        images[index].style.opacity = 1; // Показываем следующее изображение
+    }
+
+    // Показ следующего изображения сразу после загрузки
+    showNextImage();
+
+    setInterval(showNextImage, 6000); // Изменить изображение каждые 3 секунды
+});
+document.addEventListener("DOMContentLoaded", function() {
+    const navbar = document.querySelector('nav');
+    let lastScrollTop = 0;
+
+    window.addEventListener('scroll', function() {
+        let currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (currentScrollTop > lastScrollTop) {
+            // Scrolling down
+            navbar.classList.add('nav-hidden');
+        } else {
+            // Scrolling up
+            navbar.classList.remove('nav-hidden');
+        }
+
+        lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; // For Mobile or negative scrolling
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Проверьте, если редирект с успешного логина
+    const params = new URLSearchParams(window.location.search);
+    if (params.has('login') && params.get('login') === 'success') {
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            customClass: {
+                container:'custom-toast-container'
+            },
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer);
+                toast.addEventListener('mouseleave', Swal.resumeTimer);
+            },
+            didClose:()=>{
+                window.location.href = "/";
+            }
+        });
+
+        Toast.fire({
+            icon: 'success',
+            title: 'Signed in successfully'
+        });
+    }
+});
