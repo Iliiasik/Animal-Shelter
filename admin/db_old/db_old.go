250c74f8733cf3d4bab95a7445e666290e7d0d8a
Iliias Bayishev <168758422+Iliiasik@users.noreply.github.com>
2024-12-23 00:56:39 +0600

    admin panel redirect v7

--- admin/db_old/db_old.go
+++ admin/db_old/db_old.go
@@ -2,10 +2,9 @@ package db_old
 
 import (
 	"fmt"
+	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 	"log"
 	"os"
 )
@@ -15,18 +14,15 @@ func ConnectOldDB() *gorm.DB {
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
-
 	dsn := fmt.Sprintf(
-		"host=34.16.104.66 user=%s password=%s dbname=%s sslmode=disable client_encoding=UTF8",
+		"host=34.16.104.66 user=%s password=%s dbname=%s sslmode=disable",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 	)
-
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open("postgres", dsn)
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
 	}
-
 	return db
 }
